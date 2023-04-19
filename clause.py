531c7ef84b8f892e383d598a9b1d859bb1c336f2
pross1312 <dangvinhtuong12@gmail.com>
2023-04-19 10:29:13 +0700

    remove

--- clause.py
+++ clause.py
@@ -9,11 +9,11 @@ def start_parse_symbol(clause, start_index):
         if clause[end] == '(' or clause[end] == ',' or clause[end] == ')':
             break
     name = clause[start_index : end]
-    str.strip(name)
+    name = str.strip(name)
     # if end index is an open parentheses then it's a function or predicate
 
     if clause[end] == '(':
-        sym_type = Symbol_FOL.Symbol_Type.FUNCTION
+        sym_type = Symbol_FOL.Symbol_Type.COMPOUND
         args = []
         # then we continue to parse all arguments
         index = end + 1
@@ -37,14 +37,13 @@ def start_parse_symbol(clause, start_index):
         args = None
         end_index_of_symbol = end
     symbol = Symbol_FOL.Symbol(name, sym_type, args)
-    print('parse symbol: ', symbol)
+    # print('parse symbol: ', symbol)
     return symbol, end_index_of_symbol 
 
 def parse_clause(clause : str):
     index = 0       # current index in clause 
     # operator (parentheses / if (:-) / and (,) or (;) ...)
     # list of tokens returned (each element is either symbol or operator)   
-    operator = ['(', ':-', ',', ';', ')']
     tokens = []     
     str.strip(clause) # remove unnecessary white spaces 
     while index < len(clause):
@@ -56,7 +55,7 @@ def parse_clause(clause : str):
         # call start_parse_symbol
         if str.isalpha(clause[index]): 
             symbol, index = start_parse_symbol(clause, index)
-            print('clause symbol: ', symbol)
+            # print('clause symbol: ', symbol)
             tokens.append(symbol)
         # probably start of operator like ( | ) | :- | ; | ,...
         else:
@@ -64,11 +63,47 @@ def parse_clause(clause : str):
         index += 1
     return tokens 
 
+# a clause in prolog has 2 part: head and body
+# husband(Person, Wife)   :- married(Person, Wife) , (male(Person) ; female(Wife)).
+# husband(Person, Wife)
+class Clause:
+    def __init__(self, head, body) -> None:
+        self.head = head
+        self.body = body 
+
+    # parse and return a Clause object 
+    def parse(clause : str):
+        # split head and body
+        tokens = clause.split(':-')
+        if len(tokens) > 2 or len(tokens) < 1:
+            raise Exception("Invalid clause")
+        head = parse_clause(tokens[0])
+        if (len(head) > 1):
+            raise Exception("Invalid clause")
+        if len(tokens) == 1:
+            tail = True
+        else:
+            tail = parse_clause(tokens[1])
+        return Clause(head, tail)
+
+    def __str__(self) -> str:
+        s = str(self.head[0])
+        if self.body != True:
+            s += ' :- '
+            for token in self.body:
+                if token == ',':
+                    s += 'and '
+                elif token == ';':
+                    s += 'or '
+                else:
+                    s += str(token) + ' '
+        return s 
+
+inp = 'husband (Person, Wife)   :- married(Person, Wife) , (male(Person) ; female(Wife))'
 
 input_test = 'husband(Person, Wife)'
 input_test2 = 'married(Person, Wife) , (male(Person) ; female(Wife))'
-tokens = parse_clause(input_test2)
-print()
-print()
-for t in tokens:
-    print(t)
+
+c = Clause.parse(inp)
+print(c)
+
