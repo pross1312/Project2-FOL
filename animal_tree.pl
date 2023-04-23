bda861c93b49f49a6cbd3a4600c6af3dc70b286e
gigachad0209 <97180047+gigachad0209@users.noreply.github.com>
2023-04-23 23:22:12 +0700

    Add files via upload
    50% complete, will be done by tomorrow

---
+++ animal_tree.pl
@@ -0,0 +1,9 @@
+ishuy(X) :- deptrai(X) ; human(X).
+human(X) :- male(X) , lmao(X).
+deptrai(X) :- male(X) , haha(X).
+chad(X) :- male(X) , lmao(X) , handsome(X).
+
+male('Tuong').
+
+haha('Tuong').
+
