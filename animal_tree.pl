5a07994c1777884927d73a83b67d6070b77c5c98
gigachad0209 <97180047+gigachad0209@users.noreply.github.com>
2023-04-23 23:23:17 +0700

    Delete animal_tree.pl

--- animal_tree.pl
+++
@@ -1,9 +0,0 @@
-ishuy(X) :- deptrai(X) ; human(X).
-human(X) :- male(X) , lmao(X).
-deptrai(X) :- male(X) , haha(X).
-chad(X) :- male(X) , lmao(X) , handsome(X).
-
-male('Tuong').
-
-haha('Tuong').
-
