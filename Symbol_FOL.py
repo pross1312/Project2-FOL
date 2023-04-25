2f589dc8002ef0f395c6e3477867397e1dc3c36a
pross1312 <dangvinhtuong12@gmail.com>
2023-04-25 20:26:40 +0700

    change

--- Symbol_FOL.py
+++ Symbol_FOL.py
@@ -138,15 +138,30 @@ def Unify_Var(var, x, substitutes) -> list:
         return None
     return substitutes + [(var, x)]
 
-def print_substitutes(substitutes):
-    format_string = '({0} / {1})'
-    substitutes_strings = []
-    if substitutes.__class__.__name__ == 'list':
-        for sub in substitutes:
-            substitutes_strings.append(format_string.format(str(sub[0]), str(sub[1])))
+# New output
+def check_if_Var(sub, list_args):
+    for i in list_args:
+        if sub.type == i.type == Symbol_Type(2) and sub.name == i.name:
+            return True
+    return False
+
+def convert_Output(substitutes, list_args):
+    if len(substitutes)==0:
+        print("True")
+    elif len(substitutes)==1:
+        print("True")
+        print(substitutes[0][0].name + ' = ' + substitutes[0][1].name)
     else:
-        substitutes_strings.append(format_string.format(str(substitutes[0]), str(substitutes[1])))
-    print('[' + ', '.join(substitutes_strings) + ']')
+        print("True")
+        for i in substitutes:
+            if  check_if_Var(i[0], list_args):
+                uni = i[1]
+                while(uni.type != Symbol_Type(1)):
+                    for j in substitutes:
+                        if j[0] == uni:
+                            uni=j[1]
+            
+                print(i[0].name + " = "+  uni.name)
 
 # New output
 def check_if_Var(sub, list_args):
@@ -172,3 +187,4 @@ def convert_Output(substitutes, list_args):
                             uni=j[1]
             
                 print(i[0].name + " = "+  uni.name)
+
